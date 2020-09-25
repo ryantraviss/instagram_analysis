ca861d0bfc84f68326f4e220f2a9e8d378e72876
Ryan Traviss <55143606+ryantraviss@users.noreply.github.com>
2020-09-25 18:29:56 +0100

    only uses my messages now

--- analysis_word.py
+++ analysis_word.py
@@ -2,6 +2,9 @@
 
 import util
 
+print("What is your username?")
+USERNAME = input(">>>")
+
 class AnalysisWord:
     def __init__(self,path="",comments_filename = "comments.json", 
                  messages_filename = "messages.json", comments=True, messages=True):
@@ -20,22 +23,23 @@ class AnalysisWord:
         if self.messages:
             for x in range(len(self.messages_json_data)):
                 for y in range(len(self.messages_json_data[x]["conversation"])):
-                    if "text" in self.messages_json_data[x]["conversation"][y].keys():
-                        if self.messages_json_data[x]["conversation"][y]["text"] is not None:
-                            word_data.extend(self.messages_json_data[x]["conversation"][y]["text"].split())
-                        
+                    if self.messages_json_data[x]["conversation"][y]["sender"] == USERNAME:
+                        if "text" in self.messages_json_data[x]["conversation"][y].keys():
+                            if self.messages_json_data[x]["conversation"][y]["text"] is not None:
+                                word_data.extend(self.messages_json_data[x]["conversation"][y]["text"].split())
+                            
         return word_data
     
     def top_words(self):
         words = analysis_object._data()
         
-        with open("words_to_remove.txt","r") as f:
+        with open("words_to_remove.txt","r") as f: #currently top 100 words in English language
             words_to_remove = f.read().split()
             
         for word in words_to_remove:
             words = list(filter((word).__ne__, words))
                 
-        util.table(words,sort_by_likes=True,max_rows=100)
+        util.table(words,sort_by_likes=True,max_rows=100,sort="asc")
         
 analysis_object = AnalysisWord(messages=True)
 analysis_object.top_words()
